ea79e26230770c30a8a5a0682d9ca82cc05d15fc
Joanna Cruz <joannacruz@csu.fullerton.edu>
2021-09-09 20:23:49 -0700

    First commit

--- prob01/main.cc
+++ prob01/main.cc
@@ -5,12 +5,91 @@
 #include "pizza.h"
 
 using graphics::Image;
+using std::endl;
+
+
+
+// Function prototypes
+void AddCrust(graphics::Image& pizza);
+
+void AddSauce(graphics::Image& pizza);
+
+void AddCheese(graphics::Image& pizza);
+
+void AddPepperoni(graphics::Image& pizza);
+
+void AddOnion(graphics::Image& pizza);
+
+void AddJalapeno(graphics::Image& pizza);
+
+
+
 
 int main() {
+
+  char answer = ' '; 
+
+// image file name 
+  Image my_image(500, 500);
+  my_image.SaveImageBmp("pizza.bmp");
+
+
+
+
   std::cout << "Welcome to PizzaMaker 2.0!" << std::endl;
+  std::cout << "\nWould you like a pizza?"
+            << "\n(y/n): "; 
+  std::cin >> answer;
+  if ((answer=='Y') || (answer=='y')) {
+    AddCrust(my_image);
+  } else {
+    return 0;
+  }
+  
+  std::cout << "\nWould you like tomato sauce" << "\n(y/n): ";
+  std::cin >> answer;
+  if ((answer=='Y') || (answer=='y')) {
+    AddSauce(my_image);
+  } else {
+    return 0;
+  }
+  
+  std::cout << "\nWould you like cheese with that?" << "\n(y/n): ";
+  std::cin >> answer;
+  if ((answer=='Y') || (answer=='y')) {
+    AddCheese(my_image);
+  } else {
+    return 0;
+  }
+  
+  std::cout << "\nWould you like pepperoni" << "\n(y/n): ";
+  std::cin >> answer;
+  if ((answer=='Y') || (answer=='y')) {
+    AddPepperoni(my_image);
+  } else {
+    return 0;
+  }
+  
+  std::cout << "\nWould you like jalapeno with that?" << "\n(y/n): ";
+  std::cin >> answer;
+  if ((answer=='Y') || (answer=='y')) {
+    AddJalapeno(my_image);
+  } else {
+    return 0;
+  }
+  
 
-  // Add your code here to ask the user what kind of pizza they want,
-  // and create that pizza for them.
-
+  std::cout << "\nWould you like onion?" << "\n(y/n): "; 
+  std::cin >> answer;
+  if ((answer=='Y') || (answer=='y')) {
+    AddOnion(my_image);
+  } else {
+    return 0;
+  }
+  
+  std::cout << "\nGreat! Your pizza is at pizza.bmp. Enjoy!" << std::endl; 
+  
+  
+  
   return 0;
 }
