6740cf452e0992f02919ac6f96662ed85fd0c0a1
Joanna Cruz <joannacruz@csu.fullerton.edu>
2021-09-10 23:45:48 -0700

    updated for loops and initialized variables

--- prob03/main.cc
+++ prob03/main.cc
@@ -1,18 +1,41 @@
 #include <iostream>
 #include <string>
 
-std::string 
+
+int main() {
+std::string toppings; 
+std::int pepperoni;
+std::int onion;
+std::int jalapeno;
 
 std::cout << "How many pepperoni do you want? " << std::endl;
 std::cin >> pepperoni; 
 
-std::cout << "\nHow many onions do you want? "
+std::cout << "\nHow many onions do you want? ";
 std::cin >> onion; 
 
-std::cout << "\nHow many jalapenos do you want? "
+std::cout << "\nHow many jalapenos do you want? ";
 std::cin >> jalapeno;
 
-std::cout << "Here's your pizza!\n"
+std::cout << "Here's your pizza!\n";
 
-std::cout << "Pizza with: "
-std::cout << 
+
+for (int i = 0; i<pepperoni; i++) {
+  toppings = toppings + "pepperoni";
+  }
+  
+ 
+for (int i = 0; i<onion; i++) {
+  toppings = toppings + "onion";
+  } 
+  
+  
+for (int i = 0; i<jalapeno; i++) {
+  toppings = toppings + "jalapeno";
+  }
+std::cout << "Pizza with: " +toppings;
+ else {
+ std::cout << "Looks like you weren't hungry. Maybe next time?";
+ }
+ 
+ } 
